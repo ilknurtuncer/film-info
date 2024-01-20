afe6ac41774b267aa50eadf7242941c02b7c8ec8
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2024-01-20 14:13:30 +0300

    video

---
+++ src/components/Video.jsx
@@ -0,0 +1,19 @@
+const Video = ({ videoKey }) => {
+    return (
+      <div
+        className="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden rounded-xl"
+        style={{ paddingTop: "56.25%" }}
+      >
+        <iframe
+          className="embed-responsive-item absolute top-0 right-0 bottom-0 left-0 h-full w-full"
+          src={`https://www.youtube.com/embed/${videoKey}?autoplay=1&mute=1`}
+          allowFullScreen
+          title="YouTube video"
+          data-gtm-yt-inspected-2340190_699="true"
+          id={240632615}
+        />
+      </div>
+    );
+  };
+  
+  export default Video;
